aa892f9a5e357c5645eb7be1551c1c00ee2b7f58
Giovana Birck <gmattosbirck@my.bcit.ca>
2023-03-29 12:58:15 -0700

    button component updated

--- components/button-primary/index.js
+++ components/button-primary/index.js
@@ -3,7 +3,10 @@ import styles from './ButtonPrimary.module.css'
 export default function ButtonPrimary() {
     return(
         <>
-            <button className={styles.btn__primary}>Click Me!</button>
+            <div className={styles.btn}>
+                <div className={styles.btn__primary}>Click Me!</div>
+                <div className={styles.btn__background}>Click Me!</div>
+            </div>
         </>
     )
 }
