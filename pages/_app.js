b947a07ebdfb0d027d579e886721e54a0c3cbce4
Giovana Birck <gmattosbirck@my.bcit.ca>
2023-03-30 14:37:36 -0700

    truculenta added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import { Truculenta } from 'next/font/google'
+
+const truculenta = Truculenta({ subsets: ['latin'] })
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
