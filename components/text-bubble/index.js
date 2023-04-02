8b29ab836cdf434bdc050690fa094f5e1d5bad8b
Corey J Rutt <cjw.rutt@gmail.com>
2023-04-02 15:33:26 -0700

    Updated TextBubble component

--- components/text-bubble/index.js
+++ components/text-bubble/index.js
@@ -1,9 +1,60 @@
 import Image from 'next/image'
 
-export default function TextBubble() {
-    return (
-        <>
-          <Image src='/text-bubble.png' height={365} width={800} />  
-        </>
-    )
+const quizStyle = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  flexDirection: 'row',
+  width: '100%',
+  height: '100%',
+}
+
+const learningStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  width: '100%',
+  height: '100%',
+}
+
+export default function TextBubble({ component }) {
+  const renderContent = () => {
+    switch (component) {
+      case 'quiz':
+        return (
+          <div style={quizStyle}>
+            <button>Button</button>
+            <button>Button</button>
+            <button>Button</button>
+          </div>
+        )
+      case 'learning':
+        return (
+          <div style={learningStyle}>
+            <p>Your text content here</p>
+            <Image src='/imagehere.png' height={200} width={200} />
+          </div>
+        )
+      default:
+        return null
+    }
+  }
+
+  return (
+    <div style={{ position: 'relative', width: '800px', height: '365px' }}>
+      <Image src='/text-bubble.png' height={365} width={800} />
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {renderContent()}
+      </div>
+    </div>
+  )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,4 @@
 import '../styles/globals.css'
-import { Truculenta } from 'next/font/google'
-
-const truculenta = Truculenta({ subsets: ['latin'] })
-
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
