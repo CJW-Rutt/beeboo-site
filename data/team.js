ef545961dad57ee87b87b9a63e6f8a08cb9f83a9
jnip2 <jnip2@my.bcit.ca>
2023-04-03 13:22:10 -0700

    add data template

---
+++ data/team.js
@@ -0,0 +1,53 @@
+export const teamInfo = {
+    giovana: {
+        name: "Giovana Birck",
+        description: "",
+        socialMedia: [
+            {
+                platform: "LinkedIn",
+                link: "",
+                img: ""
+            },
+            {
+                platform: "GitHub",
+                link: "",
+                img: ""
+            }
+        ],
+        portfolio: ""
+    },
+    corey: {
+        name: "Corey Rutt",
+        description: "",
+        socialMedia: [
+            {
+                platform: "LinkedIn",
+                link: "",
+                img: ""
+            },
+            {
+                platform: "GitHub",
+                link: "",
+                img: ""
+            }
+        ],
+        portfolio: ""
+    },
+    joey: {
+        name: "Joey Nip",
+        description: "",
+        socialMedia: [
+            {
+                platform: "LinkedIn",
+                link: "https://www.linkedin.com/in/joey-nip/",
+                img: ""
+            },
+            {
+                platform: "GitHub",
+                link: "",
+                img: ""
+            }
+        ],
+        portfolio: ""
+    }  
+}
