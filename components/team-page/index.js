6c88466e5761867c39965ddc030849b1c90f3d47
jnip2 <jnip2@my.bcit.ca>
2023-04-03 14:07:19 -0700

    show member names through map

--- components/team-page/index.js
+++ components/team-page/index.js
@@ -1,6 +1,22 @@
+import { useState } from "react"
+import { teamInfo } from "../../data/team"
+
 export default function TeamPage() {
+
+    const [teamMember, setTeamMember] = useState("")
+    const [data, setData] = useState({ teamInfo })
+
     return (
         <>
+            {
+                data.teamInfo && data.teamInfo.map((info, index) => {
+                    return( 
+                        <div>
+                            {info.name}
+                        </div>
+                    )
+                })
+            }
         </>
     )
 }
