1ff53c332a5cbf1a4bc5755e591776a6597dd370
jnip2 <jnip2@my.bcit.ca>
2023-04-03 14:45:49 -0700

    remove unused useState

--- components/team-page/index.js
+++ components/team-page/index.js
@@ -4,7 +4,6 @@ import TeamMember from "../team-member"
 
 export default function TeamPage() {
 
-    const [teamMember, setTeamMember] = useState("nobody")
     const [chooseMember, setChooseMember] = useState(true)
     const [data, setData] = useState({ teamInfo })
     const [memberData, setMemberData] = useState()
@@ -16,7 +15,6 @@ export default function TeamPage() {
                     data.teamInfo && data.teamInfo.map((info, index) => {
                         return (
                             <div key={index} onClick={() => {
-                                setTeamMember(info.name)
                                 setMemberData(teamInfo[index])
                                 setChooseMember(false)
                             }}>
