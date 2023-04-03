3d9df6057d48ecb6f7106acb9ab0b188d540eb91
jnip2 <jnip2@my.bcit.ca>
2023-04-03 14:44:08 -0700

    build function return name and description

---
+++ components/team-member/index.js
@@ -0,0 +1,8 @@
+export default function TeamMember({name, avatar, description, socialMedia, portfolio}) {
+    return (
+        <>
+            <h1>{name}</h1>
+            <p>{description}</p>
+        </>
+    )
+}
