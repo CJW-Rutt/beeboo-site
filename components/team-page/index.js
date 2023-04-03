9ce8a961924895a9276ce50fbe51ba17da6d0139
jnip2 <jnip2@my.bcit.ca>
2023-04-03 14:44:29 -0700

    show team member names, return member info

--- components/team-page/index.js
+++ components/team-page/index.js
@@ -1,21 +1,33 @@
 import { useState } from "react"
 import { teamInfo } from "../../data/team"
+import TeamMember from "../team-member"
 
 export default function TeamPage() {
 
-    const [teamMember, setTeamMember] = useState("")
+    const [teamMember, setTeamMember] = useState("nobody")
+    const [chooseMember, setChooseMember] = useState(true)
     const [data, setData] = useState({ teamInfo })
+    const [memberData, setMemberData] = useState()
 
     return (
         <>
             {
-                data.teamInfo && data.teamInfo.map((info, index) => {
-                    return( 
-                        <div>
-                            {info.name}
-                        </div>
-                    )
-                })
+                chooseMember ?
+                    data.teamInfo && data.teamInfo.map((info, index) => {
+                        return (
+                            <div key={index} onClick={() => {
+                                setTeamMember(info.name)
+                                setMemberData(teamInfo[index])
+                                setChooseMember(false)
+                            }}>
+                                {info.name}
+                            </div>
+                        )
+                    })
+                    :
+                    <>
+                        <TeamMember name={memberData.name} description={memberData.description} />
+                    </>
             }
         </>
     )
