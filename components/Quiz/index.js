cbf7ae4104471b36696362d16b134cb2f1079fea
Corey <cjw.rutt@gmail.com>
2023-04-03 16:01:39 -0700

    More on quiz

--- components/Quiz/index.js
+++ components/Quiz/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './Quiz.module.css';
 import { quizButtons } from '../../data/quiz.js';
 import { quizQuestions } from '../../data/quiz.js';
@@ -11,6 +11,13 @@ export default function Quiz() {
   const [selectedAnswers, setSelectedAnswers] = useState([]);
   const [resultPage, setResultPage] = useState(null);
 
+  useEffect(() => {
+    if (currentPage === 'quizPageThree' && selectedAnswers.length === 3 ) {
+      determineResultPage();
+    }
+  }, [currentPage, selectedAnswers]);
+  
+
   const handleClick = (answer) => {
     setSelectedAnswers([...selectedAnswers, answer]);
     setFadeOut(true);
@@ -21,8 +28,6 @@ export default function Quiz() {
         setCurrentPage('quizPageTwo');
       } else if (currentPage === 'quizPageTwo') {
         setCurrentPage('quizPageThree');
-      } else if (currentPage === 'quizPageThree') {
-        determineResultPage();
       }
       setFadeOut(false);
     }, 500);
@@ -33,29 +38,65 @@ export default function Quiz() {
   };
 
   const determineResultPage = () => {
-    const numberOfAnswer1 = selectedAnswers.filter((answer) => answer === '1').length;
-    if (numberOfAnswer1 >= 2) {
-      setResultPage('ResultPage1');
-    } else {
-      setResultPage('ResultPage2');
-    }
+  const answerCounts = { 1: 0, 2: 0, 3: 0 };
+  selectedAnswers.forEach((answer) => {
+    answerCounts[answer]++;
+  });
 
-  };
+  if (answerCounts['1'] >= 2) {
+    setResultPage('ResultPage1');
+  } else if (answerCounts['2'] >= 2) {
+    setResultPage('ResultPage2');
+  } else if (answerCounts['3'] >= 2) {
+    setResultPage('ResultPage3');
+  } else {
+    setResultPage('ResultPage4');
+  }
+};
+
 
   const currentQuestion = quizQuestions[currentPage]?.[0];
 
   if (showIntro) {
     return (
       <div className={styles.introContainer}>
-        <h1>Introduction Headline</h1>
-        <p>Introduction paragraph.</p>
-        <button onClick={handleIntroButtonClick}>Start Quiz</button>
+        <h1>It's Quiz Time!</h1>
+        <h2>Find out how ready you are to take care of our friends the mason bees!</h2>
+        <p>Mason bees are important pollinators that play a crucial role in our ecosystem. By learning how to take care of them, we can help these tiny creatures thrive and make our world a better place. </p>
+        <p>In this quiz, we'll test what you learned on BeeBoo so far to see if you can create a safe and healthy environment for mason bees to live and reproduce.</p>
+        <p>So, get ready to put your thinking caps on and let's dive into the world of mason bees!</p>
+        <div className={styles.landingButtonContainer}>
+          <button onClick={handleIntroButtonClick}>Take the Quiz!</button>
+        </div>
       </div>
     );
   } else if (resultPage) {
     return (
       <div className={styles.resultContainer}>
-        <h1>{resultPage}</h1>
+        {resultPage === 'ResultPage1' && (
+          <div>
+            <h1>Result Page 1</h1>
+            <p>Content for result page 1.</p>
+          </div>
+        )}
+        {resultPage === 'ResultPage2' && (
+          <div>
+            <h1>Result Page 2</h1>
+            <p>Content for result page 2.</p>
+          </div>
+        )}
+        {resultPage === 'ResultPage3' && (
+          <div>
+            <h1>Result Page 3</h1>
+            <p>Content for result page 3.</p>
+          </div>
+        )}
+        {resultPage === 'ResultPage4' && (
+          <div>
+            <h1>Result Page 4</h1>
+            <p>Content for result page 4.</p>
+          </div>
+        )}
       </div>
     );
   } else {
