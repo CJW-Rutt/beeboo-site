16b8b24cca5f007744815070ad2818c876f34030
jnip2 <jnip2@my.bcit.ca>
2023-04-03 15:14:36 -0700

    add image of avatar

--- components/team-member/index.js
+++ components/team-member/index.js
@@ -1,8 +1,11 @@
+import Image from "next/image"
+
 export default function TeamMember({name, avatar, description, socialMedia, portfolio}) {
     return (
         <>
             <h1>{name}</h1>
             <p>{description}</p>
+            <Image src={avatar} height={141} width={144} />
         </>
     )
 }
