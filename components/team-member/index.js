b41d63122aa5cdbf7038a8e7d1e986696203fd9e
jnip2 <jnip2@my.bcit.ca>
2023-04-03 15:15:36 -0700

    adjust code structure

--- components/team-member/index.js
+++ components/team-member/index.js
@@ -1,11 +1,11 @@
 import Image from "next/image"
 
-export default function TeamMember({name, avatar, description, socialMedia, portfolio}) {
+export default function TeamMember({ name, avatar, description, socialMedia, portfolio }) {
     return (
         <>
+            <Image src={avatar} height={141} width={144} />
             <h1>{name}</h1>
             <p>{description}</p>
-            <Image src={avatar} height={141} width={144} />
         </>
     )
 }
