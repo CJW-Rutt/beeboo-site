bd624bdc86d7395806921f9111bddcfd1002d7cc
Corey J Rutt <cjw.rutt@gmail.com>
2023-04-16 13:45:35 -0700

    Updated prev, next, close and testing.js to accomodate changes to button functionality. Working on planting page.

--- components/Previous/index.js
+++ components/Previous/index.js
@@ -1,9 +1,9 @@
 import styles from './Previous.module.css'
 
-export default function Previous() {
+export default function Previous({ onClick }) {
     return(
         <>
-            <div className={styles.btn}>
+            <div className={styles.btn} onClick={onClick}>
                 <div className={styles.btn__arrow}></div>
                 <div className={styles.btn__background}></div>
             </div>
