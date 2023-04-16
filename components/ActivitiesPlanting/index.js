6d1bd368c718fed0bb843f2b6fe74ec5177ffa4f
Corey J Rutt <cjw.rutt@gmail.com>
2023-04-16 16:19:02 -0700

    Finished Activities: Planting

--- components/ActivitiesPlanting/index.js
+++ components/ActivitiesPlanting/index.js
@@ -32,8 +32,12 @@ export default function ActivitiesPlanting({ onClose }) {
                 { 
                     step == 0 && (
                         <div className={styles.introContainer}>
-                            <h1></h1>
-                            <button className={styles.plantingStart} onClick={handleStartPlanting}>Start Planting</button>
+                                <h1>Bee-come a Flower Planting Pro</h1>
+                                <h2>Help Save Bees One Bloom at a Time!</h2>
+                                <p>Bees are important pollinators that help plants grow and produce the food we eat.<br />Unfortunately, bees are facing many challenges, including habitat loss and pesticide use.</p>
+                                <h2>But there's something you can do to help bees: plant flowers!<br />Let's go over how to plant flowers to attract bees and help them thrive.</h2>
+                            <div className={styles.plantingStart} onClick={handleStartPlanting}>Let's Get Planting!</div>
+                            <div className={styles.plantingStartBackground}></div>
                         </div>
                     )
                 }
@@ -41,11 +45,28 @@ export default function ActivitiesPlanting({ onClose }) {
                     step == 1 && (
                         <div className={styles.innerContentContainer}>
                             <div className={styles.contentColLeft}>
-                                <h1>Title Here Page 1</h1>
-                                <p>Description Text</p>
+                                <h1>Step 1:</h1>
+                                <h2>Choosing the right flowers!</h2>
+                                <p>Bees are attracted to flowers that are brightly coloured, have a strong fragrance, and are rich in nectar and pollen. </p>
+                                <p>Some of the best flowers to plant for bees include:</p>
+                                <div className={styles.step1List}>
+                                    <ul>
+                                        <li>Sunflowers</li>
+                                        <li>Lavender</li>
+                                        <li>Marigolds</li>
+                                        <li>Daisies</li>
+                                    </ul>
+                                    <ul>
+                                        <li>Coneflowers</li>
+                                        <li>Cosmos</li>
+                                        <li>Black-eyed Susans</li>
+                                        <li>Wildflowers</li>
+                                    </ul>
+                                </div>
+                                <p>Check out your local nursery or garden centre, or you can start them from seed.</p>      
                             </div>
                             <div className={styles.contentColRight}>
-                                <Image src='/mascot/happy.svg' alt="happy mascot" width={250} height={250} />
+                                <Image src='/planting/sunflower.png' alt="happy mascot" width={208} height={208} />
                             </div>
                         </div>
                     )
@@ -54,11 +75,72 @@ export default function ActivitiesPlanting({ onClose }) {
                     step == 2 && (
                         <div className={styles.innerContentContainer}>
                             <div className={styles.contentColLeft}>
-                                <h1>Title Here Page 2</h1>
-                                <p>Description Text Page 2</p>
+                                <h1>Step 2:</h1>
+                                <h2>Preparing the soil!</h2>
+                                <p>Before you plant your flowers, you'll need to prepare the soil.</p>
+                                <p>Bees prefer soil that is well-drained and rich in organic matter.</p>
+                                <p>To improve the soil, you can:</p>
+                                <ul>
+                                    <li>Add compost or aged manure to the soil</li>
+                                    <li>Till the soil to loosen it up</li>
+                                    <li>Remove any weeds or debris</li>
+                                </ul>
                             </div>
                             <div className={styles.contentColRight}>
-                                <Image src='/mascot/happy.svg' alt="happy mascot" width={300} height={300} />
+                                <Image src='/planting/potting-soil.png' alt="happy mascot" width={208} height={208} />
+                            </div>
+                        </div>
+                    )
+                }
+                {
+                    step == 3 && (
+                        <div className={styles.innerContentContainer}>
+                            <div className={styles.contentColLeft}>
+                                <h1>Step 3:</h1>
+                                <h2>Plant the flowers!</h2>
+                                <p>Now it's time to plant the flowers!  Dig a hole slightly larger than the root ball of your plant. Gently remove the plant from its container and loosen the roots.</p>
+                                <p>Place the plant in the hole and backfill with soil. Water thoroughly and add a layer of mulch around the plant. </p>
+                                <p>Plant in a sunny location and space plants apart for good air circulation.</p>
+                            </div>
+                            <div className={styles.contentColRight}>
+                                <Image src='/planting/flower-pot.png' alt="happy mascot" width={208} height={208} />
+                            </div>
+                        </div>
+                    )
+                }                
+                {
+                    step == 4 && (
+                        <div className={styles.innerContentContainer}>
+                            <div className={styles.contentColLeft}>
+                                <h1>Step 4:</h1>
+                                <h2>Care for the flowers!</h2>
+                                <p>To keep the flowers healthy and attractive to bees, you'll need to care for them. Here are some tips:</p>
+                                <ul>
+                                    <li>Water the flowers regularly, especially during dry spells</li>
+                                    <li>Mulch around the plants to help retain moisture</li>
+                                    <li>Remove the dried out dead heads</li>
+                                    <li>Fertilize the flowers with a balanced fertilizer</li>
+                                    <li>Monitor the plants for pests and take action if necessary</li>
+                                </ul>
+                            </div>
+                            <div className={styles.contentColRight}>
+                                <Image src='/planting/watering-can.png' alt="happy mascot" width={208} height={208} />
+                            </div>
+                        </div>
+                    )
+                }
+                {
+                    step == 5 && (
+                        <div className={styles.innerContentContainer}>
+                            <div className={styles.contentColLeft}>
+                                <h1>Step 5:</h1>
+                                <h2>Enjoy the bees!</h2>
+                                <p>Once your flowers are in bloom, you should start to see bees visiting them.</p>
+                                <p>Sit back and enjoy the show! Watching bees pollinate flowers can be a fun and educational experience.</p>
+                                <p>By planting flowers for bees, you're helping to provide habitat and food for these important pollinators. So get outside and start planting!</p>
+                            </div>
+                            <div className={styles.contentColRight}>
+                                <Image src='/planting/bee.png' alt="happy mascot" width={208} height={208} />
                             </div>
                         </div>
                     )
@@ -66,7 +148,7 @@ export default function ActivitiesPlanting({ onClose }) {
             </div>
             <div className={styles.rightButtonCol}>
                 <Close onClick={onClose} />
-                { step > 0 && step !== 2 && <Next onClick={handleNext} /> }
+                { step > 0 && step !== 5 && <Next onClick={handleNext} /> }
             </div>
         </div>
     );
