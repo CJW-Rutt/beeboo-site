40865a6bf6a3e355c16e500e79095c5c200bddcc
jnip2 <jnip2@my.bcit.ca>
2023-04-16 17:37:26 -0700

    add TeamPage component

--- components/TeamScreen/index.js
+++ components/TeamScreen/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from "./Team.module.css"
+import TeamPage from '../team-page';
 
 export default function TeamScreen({ image }) {
 
   return (
-    <div className={styles.container}>
+    <>
+      <TeamPage />
+      <div className={styles.container}>
         <Image
-            className={styles.img}
-            src="../carousel/team-background.svg"
-            height={100}
-            width={100}
+          className={styles.img}
+          src="../carousel/team-background.svg"
+          height={100}
+          width={100}
         />
-    </div>
+      </div>
+    </>
   );
 }
